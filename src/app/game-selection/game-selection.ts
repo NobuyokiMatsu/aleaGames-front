71888c70576f6f89cd22a611b726fa234d5118c8
Florian <florian.lebon.124@gmail.com>
2026-02-10 07:07:04 +0100

    first real commit

---
+++ src/app/game-selection/game-selection.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-selection',
+  imports: [],
+  templateUrl: './game-selection.html',
+  styleUrl: './game-selection.scss',
+})
+export class GameSelection {
+
+}
